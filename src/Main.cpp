54010528ddd319ca4e02307a558411ca93b79118
Volodymyr Orlov <volodymyr.orlov@one.verizon.com>
2015-04-30 11:44:01 -0700

    Work in progress.

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,11 +1,25 @@
 #include <iostream>
 #include <algorithm>
+#include <stdlib.h>
+#include <unistd.h>
+#include <getopt.h>
 
 #include "opencv/cv.h"
 #include "opencv/highgui.h"
 
 using namespace std;
 using namespace cv;
+
+void help(){
+    printf("ihmontage - brings multiple videos of the same game into a single movie.\n");
+    printf("usage:\tihmontage [options] <1st video> <2nd video> <output path>\n");
+    printf("options: \n");
+    printf("\t--help\tdisplay this help\n");
+    printf("\t--verbose\toutput more information during video processing\n");
+    printf("\t-w\twidth of the output video\n");
+    printf("\t-h\theight of the output video\n");
+    printf("\t-c\tcodec of the output video, please find list of possible values here http://www.fourcc.org/codecs.php\n");
+}
 
 class MovingAverage {
     Mat previousFrame;
@@ -47,63 +61,151 @@ void printVideoMetadata(VideoCapture input){
     << "], Codec [" << input.get(CV_CAP_PROP_FOURCC) << "], # of frames [" << input.get(CV_CAP_PROP_FRAME_COUNT) << "]"<< endl;
 }
 
-void outputFrame(Mat &frame, VideoWriter &writer, double distance){
-    Mat output;
-    resize(frame, output, Size(400, 200));
-    writer << output;
-    putText(output, "Distance: " + std::to_string(distance), cvPoint(30,30),
-            FONT_HERSHEY_COMPLEX_SMALL, 0.8, cvScalar(200,200,250), 1, CV_AA);
-//    imshow("edges", output);
+void outputFrame(Mat &frame, VideoWriter &writer, double distance, int width, int height){
+    if(!frame.empty()){
+        Mat output;
+        resize(frame, output, Size(width, height));
+        writer << output;
+        putText(output, "Distance: " + std::to_string(distance), cvPoint(30,30),
+                FONT_HERSHEY_COMPLEX_SMALL, 0.8, cvScalar(200,200,250), 1, CV_AA);
+    //    imshow("edges", output);
+    }
 }
 
 int main (int argc, char *argv[])
 {
     MovingAverage movingAvgA(10), movingAvgB(10);
+    static int verboseFlag, helpFlag;
+    string pathA, pathB, outputPath;
+    int outputWidth = 400, outputHeight = 200, codec = CV_FOURCC('m', 'p', '4', 'v');
     
-    VideoCapture inputA(argv[1]);
+    while (1)
+    {
+        int c;
+        
+        static struct option long_options[] =
+        {
+            {"verbose", no_argument,       &verboseFlag, 1},
+            {"help", no_argument,       &helpFlag, 1},
+            {"h",    required_argument,       0, 'h'},
+            {"w",    required_argument,       0, 'w'},
+            {"c",    required_argument,       0, 'c'},
+            {0, 0, 0, 0}
+        };
+
+        int option_index = 0;
+        
+        c = getopt_long (argc, argv, "h:w:c:",
+                         long_options, &option_index);
+        
+        if (c == -1)
+            break;
+        
+        switch (c)
+        {
+            case 0:
+                if (long_options[option_index].flag != 0)
+                    break;
+                printf ("option %s", long_options[option_index].name);
+                if (optarg)
+                    printf (" with arg %s", optarg);
+                printf ("\n");
+                break;
+                
+            case 'h':
+                outputHeight = stoi(optarg);
+                break;
+                
+            case 'w':
+                outputWidth = stoi(optarg);
+                break;
+                
+            case 'c':
+                if(strnlen(optarg, 4) < 4){
+                    printf("incorrect codec\n");
+                    return -1;
+                } else {
+                    codec = CV_FOURCC(optarg[0], optarg[1], optarg[2], optarg[3]);
+                }
+                break;
+                
+            case '?':
+                break;
+                
+            default:
+                abort ();
+        }
+    }
+    
+    if(helpFlag){
+        help();
+        return 0;
+    }
+    
+    if (argc - optind < 3 ) {
+        printf("Incorrect parameters\n");
+        help();
+        return -1;
+    } else {
+        pathA = argv[optind];
+        pathB = argv[optind + 1];
+        outputPath = argv[optind + 2];
+    }
+    
+    VideoCapture inputA(pathA);
     if(!inputA.isOpened()){
-        cerr  << "Could not open [" << argv[1] << "]" << endl;
+        cerr  << "Could not open [" << pathA << "]" << endl;
         return -1;
     }
     
-    printVideoMetadata(inputA);
+    inputA.set(CV_CAP_PROP_POS_MSEC, 2000);
     
-    VideoCapture inputB(argv[2]);
+    if(verboseFlag) printVideoMetadata(inputA);
+    
+    VideoCapture inputB(pathB);
     if(!inputB.isOpened()){
-        cerr  << "Could not open [" << argv[2] << "]" << endl;
+        cerr  << "Could not open [" << pathB << "]" << endl;
         return -1;
     }
     
-    printVideoMetadata(inputB);
+    inputB.set(CV_CAP_PROP_POS_MSEC, 2000);
     
-    int framesNA = inputA.get(CV_CAP_PROP_FRAME_COUNT), framesNB = inputB.get(CV_CAP_PROP_FRAME_COUNT);
+    if(verboseFlag) printVideoMetadata(inputB);
     
-    VideoWriter output(argv[3], CV_FOURCC('m', 'p', '4', 'v'), inputA.get(CV_CAP_PROP_FPS), Size(400, 200), false);
+    VideoWriter output(outputPath, codec, inputA.get(CV_CAP_PROP_FPS), Size(outputWidth, outputHeight), false);
     if (!output.isOpened()){
-        cerr  << "Could not open " << argv[2] << " for write" << endl;
+        cerr  << "Could not open " << outputPath << " for write" << endl;
         return -1;
     }
     
     Mat edges;
     namedWindow("edges", CV_WINDOW_NORMAL);
-    resizeWindow("edges", 400, 200);
-    for(int i = 0; i < min(framesNA, framesNB); i++)
-    {
-        Mat frameA, frameB;
-        inputA >> frameA;
-        inputB >> frameB;
+    resizeWindow("edges", outputWidth, outputHeight);
+    
+    int framesA = inputA.get(CV_CAP_PROP_FRAME_COUNT) - inputA.get(CV_CAP_PROP_POS_FRAMES);
+    int framesB = inputB.get(CV_CAP_PROP_FRAME_COUNT) - inputB.get(CV_CAP_PROP_POS_FRAMES);
+    
+    bool hasMoreA = true, hasMoreB = true;
+    
+    do {
 
-        double distanceA = movingAvgA.nextFrame(frameA);
-        double distanceB = movingAvgB.nextFrame(frameB);
+        Mat frameA, frameB;
+        
+        if(hasMoreA) hasMoreA = inputA.read(frameA);
+        if(hasMoreB) hasMoreB = inputB.read(frameB);
+        
+        double distanceA = (hasMoreA)? movingAvgA.nextFrame(frameA) : 0;
+        double distanceB = (hasMoreB)? movingAvgB.nextFrame(frameB) : 0;
         
         if(distanceA > distanceB){
-            outputFrame(frameA, output, distanceA);
+            outputFrame(frameA, output, distanceA, outputWidth, outputHeight);
         } else {
-            outputFrame(frameB, output, distanceB);
+            outputFrame(frameB, output, distanceB, outputWidth, outputHeight);
         }
         
         if(waitKey(1) >= 0) break;
-    }
+        
+    } while(hasMoreA || hasMoreB);
 
     output.release();
     
